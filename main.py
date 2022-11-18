eca32276694df0d260749067daff8e27b5a49493
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-11-19 01:57:48 +0200

    raise Eception

--- main.py
+++ main.py
@@ -25,3 +25,18 @@ def div(x, y):
 
 print(div(5, 2))
 print(div(5, 0))
+# Use of Finally:
+"""The finally keyword is used in try...except blocks. It defines a block of code to run when the try...except...else block is final.
+
+The finally block will be executed no matter if the try block raises an error or not.
+
+This can be useful to close objects and clean up resources."""
+try:
+  x > 3
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+finally:
+  print("The try...except block is finished")
+
