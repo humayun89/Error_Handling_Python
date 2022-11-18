f6b8aa269c9b7dc439ad9789fa492892af4b6218
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-11-19 00:33:13 +0200

    raise Eception

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Two number :
 """" Developer usually use raise Exception to stop the code if the condition meet. """
+
 input_1=int(input("Enter a number : "))
 input_2=int(input("Enter a number : "))
 if input_1 > input_2:
     raise Exception("first number can be larger than the second number.")
 print((input_1+input_2))
+# assert :
