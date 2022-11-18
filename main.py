ca3de26fdec7bf6d34d9ac0504d1bd4acb0a938e
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-11-19 00:59:34 +0200

    raise Eception

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ if input_1 > input_2:
     raise Exception("first number can be larger than the second number.")
 print((input_1+input_2))
 # assert :
+"""" assert is used to run the code if condition meets otherwise it will stop."""
+
+input_1=int(input("Enter a number : "))
+input_2=int(input("Enter a number : "))
+assert input_1 > input_2:
+    raise Exception("first number can be larger than the second number.")
+print((input_1+input_2))
